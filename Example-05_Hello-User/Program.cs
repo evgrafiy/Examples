b78e3f8afc817b9b60375896f6f22e0913a066e5
Evgeniy Choopingit <evgrafiy@outlook.com>
2022-10-03 16:44:06 +0300

    приветствие

---
+++ Example-05_Hello-User/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Представтесь: ");
+string username = Console.Readline();
+
+if(username == "Маша");
+{
+    Console.WrteLine("привет, Маша");
+}
+else
+{
+    Console.WrteLine("Пивет, ");
+    Console.WrteLine(username;)
+}
